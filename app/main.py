66a287b9a2092f2e8105b7366a47d6775e9e5855
erika0915 <soohee2001@gmail.com>
2025-10-16 01:41:35 +0900

    feat: whisper 모델 api 구현

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, UploadFile, File
+from contextlib import asynccontextmanager
+from .services import stt_service
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #  서버 시작 시 모델 로드 함수 호출 
+    print("서버 시작 : Whisper 모델을 로드합니다...")
+    stt_service.load_model()
+    yield
+    #  서버 종료 시 정리 
+    print("서버가 종료되었습니다.")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def read_root():
+    return {"message": "Whisper ASR API 서버가 실행 중입니다."}
+
+@app.post("/transcribe/")
+async def transcribe(file: UploadFile = File(...)):
+    audio_bytes = await file.read()
+    transcription = stt_service.transcribe_audio_file(audio_bytes)
+    print(f"Transcription: {transcription}")
+    return {"transcription": transcription}
---
+++ app/services/stt_service.py
@@ -0,0 +1,42 @@
+import torch 
+from transformers import WhisperForConditionalGeneration, WhisperProcessor
+from peft import PeftModel
+import librosa
+import io
+
+MODEL_PATH="./model/"
+BASE_MODEL_NAME="openai/whisper-small"
+
+model = None
+processor = None
+
+def load_model():
+    global model, processor
+    if model is None:
+        print("🚀 Whisper 모델을 로드하는 중...")
+        processor = WhisperProcessor.from_pretrained(BASE_MODEL_NAME,language="korean", task="transcribe")
+        base_model = WhisperForConditionalGeneration.from_pretrained(
+            BASE_MODEL_NAME,
+            load_in_8bit=True,
+            device_map="auto"
+        )
+        model = PeftModel.from_pretrained(base_model, MODEL_PATH)
+        model.eval()
+        print("✅ 모델 로드 완료.")
+
+def transcribe_audio_file(audio_bytes:bytes) -> str:
+    if model is None or processor is None:
+        raise RuntimeError("모델이 로드되지 않았습니다.")
+    
+    audio_steam = io.BytesIO(audio_bytes)
+    speech_array, _ = librosa.load(audio_steam, sr=16000, mono=True)
+
+    input_features = processor(speech_array, sample_rate=16000, return_tensors="pt").input_features
+
+    if torch.cuda.is_available():
+        input_features = input_features.to("cuda")
+
+    predicted_ids = model.generate(input_features)
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
+    
+    return transcription
